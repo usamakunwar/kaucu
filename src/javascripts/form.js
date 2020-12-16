ac0f10c2871ab6e6fc268d28d649e979da93015e
Usama Kunwar <usamakunwar@gmail.com>
2020-12-16 13:55:48 +0000

    Contact Search url fix

--- src/javascripts/form.js
+++ src/javascripts/form.js
@@ -6,4 +6,4 @@ setupSelectData(['id_departure_airport', 'id_arrival_airport'], 'airports', null
 setupSelectData(['id_airline'], 'airlines', null, ['name', 'iata'], ['name'], null, null );
 setupSelectData(['id_hotel'], 'hotels', null, null, null, null, null );
 setupSelectData(['id_currency'], 'currencies', null, null, null, null, null );
-setupSelectData(['id_user'], null, '/kaucu/contact/search/', null, ['first_name','last_name'], ['id'], ['slug']);
+setupSelectData(['id_user'], null, '/contact/search/', null, ['first_name','last_name'], ['id'], ['slug']);
